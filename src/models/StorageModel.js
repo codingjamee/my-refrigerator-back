c0fa60676775c6ece3b1a42c4c0f519eb0985901
codingjamee <codingjamee@gmail.com>
2024-03-16 21:11:18 +0900

    Fix: modelRelations 위치 오류로 인해 수정

--- src/models/StorageModel.js
+++ src/models/StorageModel.js
@@ -1,9 +1,11 @@
 import { DataTypes } from "sequelize";
-import { sequelize } from "./index";
+import { sequelize } from "./index.js";
+import "./modelRelations.js";
 
 export const StorageModel = sequelize.define("storage", {
   id: {
     type: DataTypes.INTEGER,
+    primaryKey: true,
   },
   user_id: {
     type: DataTypes.UUID,
