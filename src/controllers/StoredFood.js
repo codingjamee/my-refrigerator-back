3d2c6874010c917a15908c07b4d23d7eb5bf4805
Jamee <codingjamee@gmail.com>
2024-03-25 14:30:17 +0900

    Feat: storedFood controller초안

--- src/controllers/StoredFood.js
+++ src/controllers/StoredFood.js
@@ -1,11 +1,19 @@
 import { StoredFoodModel } from "../models/StoredFoodModel";
 
 export class StoredFoodController {
-  static async getStoredFood({ foodId }) {
-    return StoredFoodModel.find({
-      where: {
-        foodId,
-      },
-    });
+  static async getStoredFood(req, res, next) {
+    try {
+      const storedFoodInfo = StoredFoodModel.find({
+        where: {
+          food_id: 123123,
+          // food_id: req.params.foodId,
+        },
+      });
+      res.json(storedFoodInfo);
+    } catch (err) {
+      console.log(err);
+      // next(err)
+      res.status(500).send("cannot get stored food data!");
+    }
   }
 }
