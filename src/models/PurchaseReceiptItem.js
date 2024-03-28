d24376a694fe58c54d769e545f4333e90b5db7bb
Jamee <codingjamee@gmail.com>
2024-03-28 10:45:51 +0900

    Feat: Index설정 - db조회성능 개선을 위해

--- src/models/PurchaseReceiptItem.js
+++ src/models/PurchaseReceiptItem.js
@@ -1,48 +1,54 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "./index.js";
 
-export const PurchaseReceiptItem = sequelize.define("purchase_receipt_item", {
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+export const PurchaseReceiptItem = sequelize.define(
+  "purchase_receipt_item",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    storage_id: {
+      type: DataTypes.INTEGER,
+    },
+    receipt_id: {
+      type: DataTypes.INTEGER,
+    },
+    food_id: {
+      type: DataTypes.INTEGER,
+    },
+    method: {
+      type: DataTypes.ENUM,
+      values: ["refrigerated", "frozen", "room_temp"],
+    },
+    amount: {
+      type: DataTypes.INTEGER,
+    },
+    quantity: {
+      type: DataTypes.STRING(100),
+    },
+    unit: {
+      type: DataTypes.STRING(20),
+    },
+    remain_amount: {
+      type: DataTypes.INTEGER,
+    },
+    image_url: {
+      type: DataTypes.STRING(255),
+    },
+    purchase_price: {
+      type: DataTypes.INTEGER,
+    },
+    expiry_date: {
+      type: DataTypes.DATE,
+    },
+    registered: {
+      type: DataTypes.BOOLEAN,
+    },
   },
-  storage_id: {
-    type: DataTypes.INTEGER,
-  },
-  receipt_id: {
-    type: DataTypes.INTEGER,
-  },
-  food_id: {
-    type: DataTypes.INTEGER,
-  },
-  method: {
-    type: DataTypes.ENUM,
-    values: ["refrigerated", "frozen", "room_temp"],
-  },
-  amount: {
-    type: DataTypes.INTEGER,
-  },
-  quantity: {
-    type: DataTypes.STRING(100),
-  },
-  unit: {
-    type: DataTypes.STRING(20),
-  },
-  remain_amount: {
-    type: DataTypes.INTEGER,
-  },
-  image_url: {
-    type: DataTypes.STRING(255),
-  },
-  purchase_price: {
-    type: DataTypes.INTEGER,
-  },
-  expiry_date: {
-    type: DataTypes.DATE,
-  },
-  registered: {
-    type: DataTypes.BOOLEAN,
-  },
-});
+  {
+    indexes: [{ fields: ["expiry_date"] }, { fields: ["purchase_price"] }],
+  }
+);
--- src/models/ReceiptModel.js
+++ src/models/ReceiptModel.js
@@ -1,23 +1,29 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "./index.js";
 
-export const ReceiptModel = sequelize.define("receiptModel", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    allowNull: false,
-    autoIncrement: true,
+export const ReceiptModel = sequelize.define(
+  "receiptModel",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.UUID,
+    },
+    purchase_location: {
+      type: DataTypes.STRING(50),
+    },
+    purchase_date: {
+      type: DataTypes.STRING,
+    },
+    total_price: {
+      type: DataTypes.INTEGER,
+    },
   },
-  user_id: {
-    type: DataTypes.UUID,
-  },
-  purchase_location: {
-    type: DataTypes.STRING(50),
-  },
-  purchase_date: {
-    type: DataTypes.STRING,
-  },
-  total_price: {
-    type: DataTypes.INTEGER,
-  },
-});
+  {
+    indexes: [{ fields: ["purchase_date"] }],
+  }
+);
