2e971b7c44a112206eb04836c256805b2d87a88f
Jamee <codingjamee@gmail.com>
2024-03-29 15:18:24 +0900

    Feat: user login signin 기능 구현 postman 성공

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,8 +1,34 @@
 import { User } from "../models/User.js";
+import { v4 } from "uuid";
 
 class UserController {
-  static async getUser({ userId }) {
-    return User.findOne({ where: { userId } });
+  static async signupUser(req, res, next) {
+    const { name, email, password } = req.body;
+    try {
+      await User.create({
+        id: v4(),
+        name,
+        email,
+        password,
+      });
+      return res.status(201).json("signup success!!");
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json("cannot signup user!");
+    }
+  }
+  static async loginUser(req, res, next) {
+    const { email, password } = req.body;
+    try {
+      const foundUser = User.findOne({
+        where: { id: email, password },
+        attributes: ["name", "email", "image_url"],
+      });
+      return res.status(200).json("signin success!!");
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json("cannot get user!");
+    }
   }
 }
 
