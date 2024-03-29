90f09577f738256bf398bd2b7df09c15d050fa21
Jamee <codingjamee@gmail.com>
2024-03-29 15:19:40 +0900

    Feat: user router signin signup구현

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,23 +3,7 @@ import { UserController } from "../controllers/User.js";
 
 const userRouter = Router();
 
-userRouter.get("/", async (req, res, next) => {
-  console.log("get userInfo");
-  try {
-    const userInfo = await UserController.getUser({ userId: "jenner" });
-    res.json(userInfo);
-    console.log(userInfo.toJSON());
-  } catch (err) {
-    next(err);
-  }
-});
-
-userRouter.post("/", async (req, res, next) => {
-  try {
-    const { username, email, password } = req.body;
-  } catch (err) {
-    next(err);
-  }
-});
+userRouter.post("/signup", UserController.signupUser);
+userRouter.post("/signin", UserController.loginUser);
 
 export { userRouter };
