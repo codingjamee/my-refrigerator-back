90e038d22dbe9c548b563c661f1e8f7f1eabdd93
Jamee <codingjamee@gmail.com>
2024-03-29 15:20:51 +0900

    Fix: StorageInfo defaultValue설정 미입력시를 위해

--- src/models/StorageInfo.js
+++ src/models/StorageInfo.js
@@ -9,26 +9,28 @@ const StorageInfo = sequelize.define("storage_info", {
   },
   storage_id: {
     type: DataTypes.INTEGER,
-    references: {
-      model: "storage",
-      key: "id",
-    },
+    // references: {
+    //   model: "storage",
+    //   key: "id",
+    // },
   },
   method: {
     type: DataTypes.ENUM,
     values: ["refrigerated", "frozen", "room_temp"],
-    allowNull: false,
+    defaultValue: "refrigerated",
   },
   remaining_amount: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    defaultValue: 0,
   },
   remaining_quantity: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    defaultValue: 1,
   },
   update_at: {
-    type: DataTypes.TIMESTAMP,
+    type: DataTypes.TIME,
     allowNull: false,
   },
 });
