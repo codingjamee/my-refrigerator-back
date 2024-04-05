66247680370c7ba9a638f601f894dad82084ee41
Jamee <codingjamee@gmail.com>
2024-04-05 16:17:27 +0900

    Feat: jwt토큰 쿠키로 응답해주는 기능

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,5 +1,6 @@
 import { User } from "../models/User.js";
 import { v4 } from "uuid";
+import jwt from "jsonwebtoken";
 
 class UserController {
   static async signupUser(req, res, next) {
@@ -18,12 +19,31 @@ class UserController {
     }
   }
   static async loginUser(req, res, next) {
+    const JWT_SECRET = process.env.JWT_SECRET;
     const { email, password } = req.body;
     try {
       const foundUser = User.findOne({
-        where: { id: email, password },
+        where: { id: email },
         attributes: ["name", "email", "image_url"],
       });
+      if (!foundUser) {
+        return res
+          .status(401)
+          .json("이메일 혹은 비밀번호가 일치하지 않습니다!");
+      }
+      const isValidPassword = await bcrypt.compare(
+        password,
+        foundUser.password
+      );
+      if (!isValidPassword) {
+        return res
+          .status(401)
+          .json("이메일 혹은 비밀번호가 일치하지 않습니다!");
+      }
+      const token = jwt.sign({ id: foundUser.email }, JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      res.cookie("token", token, { httpOnly: true });
       return res.status(200).json("signin success!!");
     } catch (err) {
       console.log(err);
