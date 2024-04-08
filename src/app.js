fcd3ffe489ff0e5203f132f2adec58ea62538601
Jamee <codingjamee@gmail.com>
2024-04-08 17:51:38 +0900

    Test: db test connection코드 삽ㅅ입

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import errorMiddleware from "./middleware/errorMiddleware.js";
 import { userRouter } from "./routers/userRouter.js";
 import { receiptRouter } from "./routers/receiptRouter.js";
 import { purchasedFoodRouter } from "./routers/purchasedFoodRouter.js";
+import { User } from "./models/User.js";
+import { v4 } from "uuid";
 // import { sequelize } from "./models/index.js";
 
 const app = express();
 
-// sequelize.sync({ force: true });
+// sequelize.sync({ force: false });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,7 +21,16 @@ app.get("/", (req, res) => {
 app.use("/api/user", userRouter);
 app.use("/api/receipt", receiptRouter);
 app.use("/api/food", purchasedFoodRouter);
-
+app.get("/api/test-connection", async (req, res) => {
+  // DB test connection 코드
+  await User.create({
+    id: v4(),
+    name: "jenner",
+    email: "codingjamaee@gmail.com",
+    password: "userconnecttest!",
+  });
+  return res.status(200).json("test");
+});
 app.use(errorMiddleware);
 
 export { app };
