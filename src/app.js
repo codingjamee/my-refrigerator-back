ec4769b18be78441a464be2ef23203a654b5c489
Jamee <codingjamee@gmail.com>
2024-04-09 14:50:28 +0900

    Fix: db접근 테스트코드 삭제

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ import errorMiddleware from "./middleware/errorMiddleware.js";
 import { userRouter } from "./routers/userRouter.js";
 import { receiptRouter } from "./routers/receiptRouter.js";
 import { purchasedFoodRouter } from "./routers/purchasedFoodRouter.js";
-import { User } from "./models/User.js";
-import { v4 } from "uuid";
 // import { sequelize } from "./models/index.js";
 
 const app = express();
@@ -21,16 +19,6 @@ app.get("/", (req, res) => {
 app.use("/api/user", userRouter);
 app.use("/api/receipt", receiptRouter);
 app.use("/api/food", purchasedFoodRouter);
-app.get("/api/test-connection", async (req, res) => {
-  // DB test connection 코드
-  await User.create({
-    id: v4(),
-    name: "jenner",
-    email: "codingjamaee@gmail.com",
-    password: "userconnecttest!",
-  });
-  return res.status(200).json("test");
-});
 app.use(errorMiddleware);
 
 export { app };
