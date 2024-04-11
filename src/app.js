f4c89866a6100c20152a3770fda0ee31676bc7d6
Jamee <codingjamee@gmail.com>
2024-04-12 02:06:25 +0900

    Feat: cookieParser추가 및
    verifyToken middleware 추가

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ import errorMiddleware from "./middleware/errorMiddleware.js";
 import { userRouter } from "./routers/userRouter.js";
 import { receiptRouter } from "./routers/receiptRouter.js";
 import { purchasedFoodRouter } from "./routers/purchasedFoodRouter.js";
+import cookieParser from "cookie-parser";
+import { verifyToken } from "./middleware/verifyToken.js";
 // import { sequelize } from "./models/index.js";
 
 const app = express();
 
 // sequelize.sync({ force: false });
+app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -17,8 +20,8 @@ app.get("/", (req, res) => {
   res.send("This is my refrigerator project back server");
 });
 app.use("/api/user", userRouter);
-app.use("/api/receipt", receiptRouter);
-app.use("/api/food", purchasedFoodRouter);
+app.use("/api/receipt", verifyToken, receiptRouter);
+app.use("/api/food", verifyToken, purchasedFoodRouter);
 app.use(errorMiddleware);
 
 export { app };
